ba8cbcd7d670e58b5d17a692416d3d9a328d6582
poor1314 <junlinc9394@gmail.com>
2025-04-06 17:49:43 -0500

    basic operation of the calc now works

--- calc.js
+++ calc.js
@@ -1,27 +1,41 @@
-function add(number1,number2){
-    return number1 + number2;
+function calculator() {
+    let current = 0;
+
+    return {
+        add(input) {
+           current += input;
+        },
+
+        subtract(input){
+            current -= input;
+        },
+
+        multiply(input){
+            current *= input;
+        },
+
+        divide(input){
+            current /= input;
+        },
+
+        reset() {
+            current = 0;
+        },
+
+        getValue(){
+            return current
+        },
+    };
 }
 
-aasdasd
 
+// const doMath= calculator();
+// doMath.add(5.3);
+// doMath.add(2);
+// console.log(doMath.getValue());
+// // doMath.reset();
+// // console.log(doMath.getValue());
 
-aasdasd
+// doMath.divide(5);
+// console.log(doMath.getValue());
 
-
-aasdasd
-
-
-aasdasd
-
-asdxcvx
-
-
-
-
-dfrgdfg
-
-dfsdf
-
-aasdasd
-
-dfsdf
