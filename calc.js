ef8c2af05f84757408e2299344e7555c2970d2c6
poor1314 <junlinc9394@gmail.com>
2025-04-06 20:10:42 -0500

    the clear function on calc display now works

--- calc.js
+++ calc.js
@@ -28,9 +28,6 @@ function calculator() {
     };
 }
 
-
-
-
 // const doMath= calculator();
 // doMath.add(5.3);
 // doMath.add(2);
@@ -40,4 +37,70 @@ function calculator() {
 
 // doMath.divide(5);
 // console.log(doMath.getValue());
+
+
+
+
+let element = document.querySelector(".buttons");
+let display = document.querySelector(".display");
+let clear = document.querySelector(".clear");
+
+// display content on calc console except clear and delete
+function displayContent(e) {
+    let target = e.target;
+    const isButton = target.tagName === "BUTTON";
+    const isClearOrDelete = target.className === "clear" || target.className === "delete";
+    
+
+    // Check if it's a button and not a clear/delete button
+    if (isButton && !isClearOrDelete) {
+        display.textContent = target.textContent;
+    }
+}
+
+
+element.addEventListener("click", function(e){
+
+        let target = e.target.textContent;
+        displayContent(e)
+        console.log(target);
+        
+
+        switch(target) {
+            case 'CLEAR': // works
+                calculator().reset();
+                display.textContent = "";
+                break;
+
+            // case 'DELETE':
+            //     console.log('');
+            //     break;
+
+            case '+':
+                calculator().add(Number(target));
+                break;
+        
+            // case '-':
+            //     console.log('');
+            //     break;
+
+            // case '*':
+            //     console.log('');
+            //     break;
+
+            // case '÷':
+            //     console.log('÷');
+            //     break;
+
+            case '=': // why is getValue = 0?
+                console.log(calculator().getValue());
+                
+                display.textContent = calculator().getValue();
+                break;
+        }
+});
+
+
+
+
 
