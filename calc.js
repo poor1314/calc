8a943f8d73cad13676507bbf620696197010433e
poor1314 <junlinc9394@gmail.com>
2025-04-10 11:20:28 -0500

    erase(){             number1 = "";             number2 = "";             operation = "";             newNumber = "";             displayScreen.textContent = "Cleared!";             displayMiniScreen.textContent = "";             current = 0;
            },

--- calc.js
+++ calc.js
@@ -1,147 +1,169 @@
+let buttonsContainer = document.querySelector(".buttons");
+let mainDisplay = document.querySelector(".display");
+let historyDisplay = document.querySelector(".displayMini");
+let clearButton = document.querySelector(".clear");
+
 function calculator() {
-    let current = 0;
+    let currentTotal = Number("");
+    let subtractFlag = true; // 1st currentTotal will be input itself
+    let multiplyFlag = true;  // 1st currentTotal * 1
+    let divideFlag = true;
     return {
-        add(number1) {
-            current += number1;
-        },
-      
-        subtract(number1){
-            current -= number1;
-        },
-
-        multiply(number1){
-            current *= number1;
+        add(currentInput) {
+            currentTotal += currentInput;
+            subtractFlag = false; 
+            multiplyFlag = false;  
+            divideFlag = false;
         },
 
-        divide(number1){
-            current /= number1
+        subtract(currentInput) {
+
+            if(subtractFlag){
+                currentTotal = currentInput;
+            }else{
+                currentTotal -= currentInput;
+            }
+            subtractFlag = false; 
+            multiplyFlag = false;  
+            divideFlag = false;
+
         },
 
-        // delete(number1){
-
-        // },
-
-        erase(){
-            number1 = "";
-            number2 = "";
-            operation = "";
-            newNumber = "";
-            displayScreen.textContent = "Cleared!";
-            displayMiniScreen.textContent = "";
-            current = 0;
-            
+        multiply(currentInput) {
+           
+            if(multiplyFlag){
+                currentTotal = currentInput;
+            }else{
+                currentTotal *= currentInput;
+            }
+            subtractFlag = false; 
+            multiplyFlag = false;  
+            divideFlag = false;
+          
         },
-     
-        getSum(){
-            return current
+
+        divide(currentInput) {
+             
+            if(divideFlag){
+                currentTotal = currentInput;
+            }else{
+                currentTotal /= currentInput;
+            }
+            subtractFlag = false; 
+            multiplyFlag = false;  
+            divideFlag = false;
+        },
+
+        getSum() {
+            return currentTotal;
+        },
+
+        erase() {
+            currentInput = "";
+            previousInput = "";
+            selectedOperator = "";
+            isNewInput = "";
+            mainDisplay.textContent = "Cleared!";
+            historyDisplay.textContent = "";
+            subtractFlag = true;
+            multiplyFlag = true;
+            divideFlag = true;
+            // this needs to be number
+            // else string + number = string 
+            // 33 + 66 = 3366
+            currentTotal = 0;
         },
     };
 }
 
-let buttonsContainer = document.querySelector(".buttons");
-let displayScreen = document.querySelector(".display");
-let displayMiniScreen = document.querySelector(".displayMini");
-let clearButton = document.querySelector(".clear");
-
-
-// // display numbers
 function displayContent(number) {
-
-    displayScreen.textContent = number;
-
+    mainDisplay.textContent = number;
 }
 
-function miniScreen(number2, operation) {
-
-
-    displayMiniScreen.textContent =  `${number2} ${operation}`;
-
+function miniScreen(previousInput, selectedOperator) {
+    historyDisplay.textContent = `${previousInput} ${selectedOperator}`;
 }
 
+let calculatorObj = calculator();
+let currentInput = "";
+let previousInput = "";
+let selectedOperator = "";
+let isNewInput;
 
+buttonsContainer.addEventListener("click", function(e) {
 
-let calculate = calculator(); 
-let number1 = "";
-let number2 = "";
-let operation = "";
-let newNumber;
+    const numberKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+    const operatorKeys = ["+", "−", "×", "÷", "="];
+    let keyPressed = e.target.textContent;
 
-buttonsContainer.addEventListener("click", function(e){
-        
-        const num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-        const operation = ["+", "-", "x","÷", ];
-        let keyPressed = e.target.textContent;
+    console.log("keyPressed", keyPressed);
 
-        console.log(keyPressed);
-        
-        // reset everything on display, and also all number value
-        if (keyPressed === "CLEAR"){
-            calculate.erase();
-        } 
+    if (keyPressed === "CLEAR") {
+        calculatorObj.erase();
+    }
 
-
-      
-        // only allow 1 dot per number
-        if ((number1.includes(".") && keyPressed === ".")){
-            number1  += ""; 
-
-        // digital accumulation 
-        }else if(num.includes(keyPressed)){
-         
-            number1  += keyPressed;
-            number2 = number1;
-            newNumber = true;
-            displayContent(number1);
-
-        }else if (number1 && operation.includes(keyPressed) && newNumber){
-            console.log("made it passed last", keyPressed);
-            
-            switch(keyPressed){
-                case "+":
-                    console.log("hhh");
-                    calculate.add(Number(number1));
-                    break;
-
-                case "-":
-                    console.log("worked?");
-                    
-                    calculate.subtract(Number(number1));
-                    break;
-                
-                case "x":
-                    calculate.multiply(Number(number1));
-                    break;
-                
-                case "÷":
-                    calculate.divide(Number(number1));
-                    break;
-
-                case "delete":
-                    console.log("check if delete works");
-                    
-                    calculate.divide(Number(number1));
-                    break;
-
-               
-
-            } 
-            // display previous number & operator 
-            displayContent(calculate.getSum());
-            miniScreen(calculate.getSum(), keyPressed);
-            
-            // a mechanic to prevent passing the if-statement logic
-            // by spamming operators 
-            // before this, if i had a number as number1, and i spam operator
-            // it will keep operating that number
-            newNumber = false;
-            // reset number1
-            number1 = "";
+    // if(previousInput &&  )
+    // miniScreen(calculatorObj.getSum(), keyPressed, currentInput, result);
+    // miniScreen(calculatorObj.getSum(), keyPressed);
  
+
+    if ((currentInput.includes(".") && keyPressed === ".")) {
+        currentInput += "";
+     
+
+    } else if (numberKeys.includes(keyPressed)) {
+
+        currentInput += keyPressed;
+        previousInput = currentInput;
+        isNewInput = true;
+        displayContent(currentInput);
+       
+        
+
+
+    } else if (currentInput && operatorKeys.includes(keyPressed) && isNewInput) {
+       
+      
+        switch (keyPressed) {
+            case "+":
+                calculatorObj.add(Number(currentInput));
+                break;
+
+            case "−":
+                calculatorObj.subtract(Number(currentInput));
+                break;
+
+            case "×":
+                calculatorObj.multiply(Number(currentInput));
+                break;
+
+            case "÷":
+                calculatorObj.divide(Number(currentInput));
+                break;
         }
 
-       
+        result = calculatorObj.getSum() + Number(previousInput);
+        mainDisplay.textContent = result;
+     
+        // display previous number & operator 
+        displayContent(calculatorObj.getSum());
+        miniScreen(calculatorObj.getSum(), keyPressed);
+
+        
+
+        // a mechanic to prevent passing the if-statement logic
+        // by spamming operators 
+        // before this, if i had a number as number1, and i spam operator
+        // it will keep operating that number
+        isNewInput = false;
+        currentInput = "";
+    }
   
-}); 
+});
 
+// fix some operations
+// add delete function
 
-
+// make miniScreen display 55 + 55 + 55 + 55 = ?
+// the input will be added to the number
+// also update the result
