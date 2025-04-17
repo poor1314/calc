f47bca17ec2cbbaff73ed9e9fdd48e44704a7aac
poor1314 <junlinc9394@gmail.com>
2025-04-17 18:29:10 -0500

    final update

--- calc.js
+++ calc.js
@@ -9,35 +9,43 @@ let historyDisplay = document.querySelector(".displayMini");
 
 calculatorButtons.addEventListener("click", function(e){
     let buttonValue = e.target.textContent;
-    const numberList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-    const operatorList = ["+", "-", "x", "÷", "="];
+    const firstOperandExist = firstOperand || (firstOperand === 0); 
+    const NUMBER_BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+    const OPERATOR_BUTTONS = ["+", "-", "x", "÷", "=", "CLEAR", "DELETE"];
+    const validButton = NUMBER_BUTTONS.includes(buttonValue) || OPERATOR_BUTTONS.includes(buttonValue);
 
-    if(numberList.includes(buttonValue)) updatePrimaryDisplay(buttonValue);
-    if(buttonValue === "=") secondOperand = currentInputBuffer;
-    
-    console.log("current buttonValue", buttonValue);
-    console.log("firstOperand", firstOperand);
-    console.log("secondOperand", secondOperand);
-    console.log("selectedOperator", selectedOperator);
-    console.log("currentInputBuffer", currentInputBuffer);
-    console.log("");   
-    
-    if(buttonValue === "CLEAR") resetCalculator();
-    else if(buttonValue === "DELETE") updatePrimaryDisplay(deleteLastInput());
-    // else if(buttonValue === "=") secondOperand = currentInputBuffer;
-    else if((firstOperand || Number(firstOperand) === 0 ) && selectedOperator && secondOperand && buttonValue === "="){
+    if(buttonValue === "CLEAR"){
+        resetCalculator();
+        return;
+    }
+    // reflect on display & update number after digit deletion 
+    if(buttonValue === "DELETE"){
+        updatePrimaryDisplay(deleteLastInput()); 
+        return;
+    }
+    // reflect on display & update appending to number 
+    if(NUMBER_BUTTONS.includes(buttonValue)){
+        updatePrimaryDisplay(appendToActiveOperand(buttonValue)); 
+        return;
+    } 
+    // avoid nesting, so if isn't a valid button, exit early and don't execute the below
+    if(!validButton)
+        return;
+
+    // perform calculation when we have firstOperand, secondOperand, an operator, and "=" is pressed 
+    if(firstOperandExist && selectedOperator && buttonValue === "="){
         secondOperand = currentInputBuffer;
         firstOperand = performOperation(Number(firstOperand), selectedOperator, Number(secondOperand)); 
         resetOperatorAndBuffer(); 
         updateHistoryPanel(buttonValue, firstOperand);
         updatePrimaryDisplay("");
-    }
-    else if(numberList.includes(buttonValue))updatePrimaryDisplay(appendToActiveOperand(buttonValue));
-    else if(operatorList.includes(buttonValue)){
+    
+    // allow operator changes and reflect live on calc
+    }else if(firstOperandExist){
         selectedOperator = buttonValue;
         updateHistoryPanel(selectedOperator, firstOperand);
         updatePrimaryDisplay(currentInputBuffer);
-    }
+    }  
 });
 
 function appendToActiveOperand(buttonValue){
@@ -55,6 +63,16 @@ function appendToActiveOperand(buttonValue){
     }
 }
 
+function deleteLastInput(){
+    if(!currentInputBuffer){
+        firstOperand = firstOperand.slice(0, -1);
+        return firstOperand;
+    }else {
+        currentInputBuffer = currentInputBuffer.slice(0, -1);
+        return currentInputBuffer;
+    }
+}
+
 function ensureSingleDecimal(number){
     if (!number.includes(".")) return number += ".";  
     return number;
@@ -65,7 +83,6 @@ function performOperation(firstOperand, selectedOperator, secondOperand){
     else if (selectedOperator === "-") return firstOperand - secondOperand;
     else if (selectedOperator === "x") return firstOperand * secondOperand;
     else if (selectedOperator === "÷") return firstOperand / secondOperand;
-   
 }
 
 function updatePrimaryDisplay(number){
@@ -76,16 +93,6 @@ function updatePrimaryDisplay(number){
 function updateHistoryPanel(buttonValue, firstOperand){
     if (buttonValue === "=")  historyDisplay.textContent = `${buttonValue} ${firstOperand}`;
     else historyDisplay.textContent = `${firstOperand} ${buttonValue}`;
-}
-
-function deleteLastInput(){
-    if(!currentInputBuffer){
-        firstOperand = firstOperand.slice(0, -1);
-        return firstOperand;
-    }else {
-        currentInputBuffer = currentInputBuffer.slice(0, -1);
-        return currentInputBuffer;
-    }
 }
 
 function resetCalculator(){
